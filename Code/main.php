7c430ed1b19542acb003f91593010616465b9152
lena.verscht <lena.verscht@rwth-aachen.de>
2018-12-18 14:07:21 +0100

    ordner für stylesheets

--- Code/main.php
+++ Code/main.php
@@ -29,8 +29,8 @@ try {
         <title>Skripte 4 Studis</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="messages.css">
+        <link rel="stylesheet" href="style/style.css">
+        <link rel="stylesheet" href="style/messages.css">
         <meta charset="utf-8">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
